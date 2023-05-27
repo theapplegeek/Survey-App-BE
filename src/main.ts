5093bca062bb0a1601e05f2d3c0ffff049c09bd0
Weibin Xu <weibin.xu@agmsolutions.net>
2023-05-27 21:46:28 +0200

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,34 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { json } from 'express';
 import { PrismaExceptionFilter } from './common/filters/prisma.filter';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: true,
     snapshot: true,
   });
+
+  // GLOBAL PIPES AND FILTERS
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaExceptionFilter());
 
+  // SET GLOBAL JSON BODY LIMIT
   app.use(json({ limit: '10mb' }));
 
+  // SET GLOBAL SECURITY
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
+
+  // GLOBAL PREFIX AND VERSIONING PREFIX
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
 
+  // SWAGGER DOCUMENTATION
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
       .setTitle('Survey API')
